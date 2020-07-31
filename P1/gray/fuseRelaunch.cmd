9e650cc12300419dbf372a3a96653a4fb49ba5d2
liyixiang99 <68842811+liyixiang99@users.noreply.github.com>
2020-07-31 13:41:11 +0800

    Add files via upload

---
+++ P1/gray/fuseRelaunch.cmd
@@ -0,0 +1,1 @@
+-intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -o "C:/verilog/gray/gray_tb_isim_beh.exe" -prj "C:/verilog/gray/gray_tb_beh.prj" "work.gray_tb" "work.glbl" 
